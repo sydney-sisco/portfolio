884e290bf4df8d14633e344916a9d457c2974028
Sydney Sisco <hello@sydsis.co>
2021-05-10 14:44:22 -0700

    fix formatting for Sam

--- src/App.js
+++ src/App.js
@@ -3,22 +3,22 @@ import IconContainer from './components/IconContainer';
 
 function App() {
   return (
-    <main>
+    <div>
       <header>
         <h1>Sydney Sisco</h1>
         <h2>Full Stack Developer</h2>
       </header>
-        <IconContainer />
-        <footer>
-          <h3>
-            Portfolio
-          </h3>
-          <h3>
-            <strong>|||||||||||||||</strong><span id="empty">|||||</span>
-          </h3>
-          <div class="arrow bounce"></div>
-        </footer>
-    </main>
+      <IconContainer />
+      <footer>
+        <h3>
+          Portfolio
+        </h3>
+        <h3>
+          <strong>|||||||||||||||</strong><span id="empty">|||||</span>
+        </h3>
+        <div class="arrow bounce"></div>
+      </footer>
+    </div>
   );
 }
 
