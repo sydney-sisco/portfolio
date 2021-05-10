c40e70eefbd966c50530f9c388bdcb54875ed8fe
Sydney Sisco <hello@sydsis.co>
2021-05-10 15:29:53 -0700

    add hook for switching pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
 import './App.css';
 import IconContainer from './components/IconContainer';
+import Projects from './components/Projects';
+
+import useVisualMode from "./hooks/useVisualMode";
+const MAIN = "MAIN";
+const PROJECTS = "PROJECTS";
 
 function App() {
+  const { mode, transition, back } = useVisualMode(
+    MAIN
+  );
+
   return (
     <div>
-      <header>
-        <h1>Sydney Sisco</h1>
-        <h2>Full Stack Developer</h2>
-      </header>
-      <IconContainer />
-      <footer>
-        <h3>
-          Portfolio
-        </h3>
-        <h3>
-          <strong>|||||||||||||||</strong><span id="empty">|||||</span>
-        </h3>
-        <div class="arrow bounce"></div>
-      </footer>
+    {mode === MAIN && 
+      <div>
+        <header>
+          <h1>Sydney Sisco</h1>
+          <h2>Full Stack Developer</h2>
+        </header>
+        <IconContainer />
+        <a href="/" onClick={(e) => {e.preventDefault(); transition(PROJECTS)}}>
+        <footer>
+          <h3>
+            Portfolio
+          </h3>
+          <h3>
+            <strong>|||||||||||||||</strong><span id="empty">|||||</span>
+          </h3>
+          <div className="arrow bounce"></div>
+        </footer>
+        </a>
+      </div>
+    }
+    {mode === PROJECTS && 
+      <Projects />
+    }
     </div>
   );
 }
---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,32 @@
+import { useState } from "react";
+
+/*
+useVisualMode manages the display mode of each appointment.
+*/
+
+export default function useVisualMode(initial) {
+  const [history, setHistory] = useState([initial]);
+
+  const transition = (newMode, replace = false) => {
+    if (replace) {
+      setHistory((prev) => [...prev.slice(0, -1), newMode]);
+    } else {
+      setHistory((prev) => [...prev, newMode]);
+    }
+  };
+
+  const back = () => {
+    if (history.length < 2) {
+      return;
+    }
+
+    setHistory((prev) => {
+      const newHistory = [...prev];
+      newHistory.pop();
+      return newHistory;
+    });
+  };
+
+  const mode = history.slice(-1)[0];
+  return { mode, transition, back };
+}
