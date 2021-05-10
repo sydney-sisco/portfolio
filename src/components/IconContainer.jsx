a3d929e0ec8203183e8f5f99b8dcf352469c2959
Sydney Sisco <hello@sydsis.co>
2021-05-10 11:04:43 -0700

    clean up boilerplate code. Add assets

---
+++ src/components/IconContainer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+// import "components/Application.scss";
+
+
+export default function IconContainer(props) {
+  return (
+    <div id="links">
+      <a href="https://www.linkedin.com/in/sydsisco/"><img src="images/linkedin.png" alt="linkedin" /></a>
+      <a href="https://github.com/sydney-sisco"><img src="images/github.png" alt="github" /></a>
+      <a href="https://keybase.io/sydneysisco"><img src="images/key.png" alt="keybase" /></a>
+      <a href="mailto:&#104;&#101;&#108;&#108;&#111;&#064;&#115;&#121;&#100;&#115;&#105;&#115;&#046;&#099;&#111;"><img src="images/email.png" alt="email" /></a> 
+    </div>
+  )
+};
